94588981371a81c94653c3e32d2ec4ffdf15ab42
BelieveMko <mukobelieve@gmail.com>
2025-12-14 21:32:38 +0200

    script to create all the tables

--- task_2.sql
+++ task_2.sql
@@ -1,21 +1,22 @@
 USE alx_book_store;
+
 CREATE TABLE IF NOT EXISTS authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(255) NOT NULL
+    author_name VARCHAR(215) NOT NULL
 );
 CREATE TABLE IF NOT EXISTS books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(130) NOT NULL,
     author_id INT,
-    price DECIMAL(10, 2) NOT NULL,
+    price DOUBLE NOT NULL,
     published_date DATE,
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 CREATE TABLE IF NOT EXISTS customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    address VARCHAR(255)
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215),
+    address TEXT
 );
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
